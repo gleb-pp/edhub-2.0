f8ad9dfa5f93429a74315a460d959cd95273c4c6
Timur Usmanov <t.usmanov@innopolis.university>
2025-06-14 17:53:39 +0300

    Bring back 'assert_assignment_exists'

--- backend/constraints.py
+++ backend/constraints.py
@@ -85,6 +85,34 @@ def check_material_exists(db_cursor, course_id: str, material_id: str) -> bool:
     return value_assert_user_exists(db_cursor, course_id, material_id) is None
 
 
+# checking whether the assignment exists in the course
+def value_assert_assignment_exists(db_cursor, course_id: str, assignment_id: str) -> Union[None, HTTPException]:
+    try:
+        assignment_id = int(assignment_id)
+    except Exception:
+        return HTTPException(status_code=400, detail="Assignment ID should be integer")
+
+    err = value_assert_course_exists(db_cursor, course_id)
+    if err is not None:
+        return err
+    db_cursor.execute("SELECT EXISTS(SELECT 1 FROM course_assignments WHERE courseid = %s AND assid = %s)", (course_id, assignment_id))
+    assignment_exists = db_cursor.fetchone()[0]
+    if not assignment_exists:
+        return HTTPException(status_code=404, detail="No assignment with provided ID in this course")
+
+
+# checking whether the assignment exists in the course
+def assert_assignment_exists(db_cursor, course_id: str, assignment_id: str):
+    err = value_assert_assignment_exists(db_cursor, course_id, assignment_id)
+    if err is not None:
+        raise err
+
+
+# checking whether the assignment exists in the course
+def check_assignment_exists(db_cursor, course_id: str, assignment_id: str) -> bool:
+    return value_assert_assignment_exists(db_cursor, course_id, assignment_id) is None
+
+
 # checking whether the user has general access to the course,
 def value_assert_course_access(db_cursor, user_email: str, course_id: str) -> Union[None, HTTPException]:
     err = value_assert_user_exists(user_email)
