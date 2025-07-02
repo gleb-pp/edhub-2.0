f0968d5855ff7aa32425d50ed17a71f21988b29f
AlfyK1s <struchkovtimur2004@gmail.com>
2025-07-02 20:39:22 +0300

    added ability  as a student to submit and view an answer, also fixed the bug of movement of the button to the second row in course window

--- frontend/src/pages/AssignmentPage.jsx
+++ frontend/src/pages/AssignmentPage.jsx
@@ -6,8 +6,14 @@ import axios from "axios"
 export default function AssignmentPage() {
   const { id, post_id } = useParams()
   const [assignmentInfo, setAssignmentInfo] = useState(null)
+  const [text,setText] = useState("")
+  const [showSubmissionForm, setShowSubmissionForm] = useState(true)
+  const [roleData, setRoleData] = useState()
+  const [ownEmail, setOwnEmail] = useState("")
+  const [mySubmission, setMySubmission] = useState(null)
 
-  
+
+
   useEffect(() => {
     const fetchAssignmentInfo = async () => {
       try{
@@ -23,8 +29,81 @@ export default function AssignmentPage() {
       }
     }
     fetchAssignmentInfo()
-  }, [id])
 
+    const fetchRoleData = async () => {
+      try{
+        const token = localStorage.getItem("access_token")
+        const res = await axios.get("/api/get_user_role", {
+          headers: { Authorization: `Bearer ${token}` },
+          params: { course_id: id },
+        })
+        setRoleData(res.data)
+      }catch(err){
+        alert("Ошибка при загрузке роли пользователя: " + (err.response?.data?.detail || err.message))
+      }
+    }
+    fetchRoleData()
+
+    const fetchEmail = async () => {
+      try {
+        const token = localStorage.getItem("access_token")
+        const res = await axios.get("/api/get_user_info", {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        setOwnEmail(res.data.email)
+      } catch (err) {
+        alert("Ошибка при загрузке email'а пользователя: " + (err.response?.data?.detail || err.message))
+      }
+    }
+    fetchEmail()
+
+    
+  }, [id])
+  const fetchMySubmission = async () => {
+      try {
+        const token = localStorage.getItem("access_token")
+        const res = await axios.get("/api/get_submission", {
+          headers: { Authorization: `Bearer ${token}` },
+          params: { course_id: id, assignment_id: post_id , student_email: ownEmail }
+        })
+        setMySubmission(res.data)
+      } catch (err) {
+        alert("Ошибка при загрузке ответа студента: " + (err.response?.data?.detail || err.message))
+      }
+  }
+  useEffect(() => {
+    if (roleData?.is_student && ownEmail) {
+      fetchMySubmission();
+    }
+  }, [roleData, ownEmail, id, post_id]);
+
+  useEffect(() => {
+    if (mySubmission) {
+    setShowSubmissionForm(false);
+    }
+  }, [mySubmission]);
+
+  const handleSubmit = async () => {
+    // if (!studentEmail.trim()) {
+    //   alert("Student's Email is required")
+    //   return
+    // }
+
+    try {
+      const token = localStorage.getItem("access_token")
+      await axios.post("/api/submit_assignment", null, {
+        headers: {Authorization: `Bearer ${token}` },
+        params:{course_id: id, assignment_id: post_id, comment: text}
+      })
+      setShowSubmissionForm(false)
+      fetchMySubmission()
+    } catch (err) {
+      alert("Ошибка при отправке ответа: " + (err.response?.data?.detail || err.message))
+    }
+    
+  }
+
+  if (!assignmentInfo) return <div>Loading assignment...</div>
   return (
     <div className="assignment-page">
       <a href="../">
@@ -34,12 +113,40 @@ export default function AssignmentPage() {
         <div className="assignment-left">
           <h1>{assignmentInfo.title}</h1>
           <p>{assignmentInfo.description}</p>
-          <p>Assignment ID : {assignmentInfo.material_id}</p>
+          <p>Assignment ID : {assignmentInfo.assignment_id}</p>
           <p>Created : {assignmentInfo.creation_time}</p>
+          <p>Current Email: {ownEmail}</p>
           <p>author : {assignmentInfo.author}</p>
         </div>
         <div className="assignment-right">
-          <h2>Submissions</h2>
+          {roleData && roleData.is_student && showSubmissionForm && !mySubmission &&(
+            <div className="student-submit-block">
+              <h2>Submit your work</h2>
+              <div>
+              <textarea 
+                placeholder="Write your submission here..."
+                value={text} onChange={(e) => setText(e.target.value)}
+                rows="10" 
+                cols="30"/>
+              <button 
+                className="submit-btn"
+                onClick={handleSubmit} 
+                disabled={!text.trim()}
+              > Submit
+              </button>
+              </div>
+            </div>
+          )}
+          {roleData && roleData.is_student && !showSubmissionForm && mySubmission && (
+            <div className="submitted-answer">
+              <div className="my-comment-title">Your submission:</div>
+              <div className="my-comment">{mySubmission.comment}</div>
+              {mySubmission.grade && (
+                <div className="my-grade">Grade: <b>{mySubmission.grade}</b> (by {mySubmission.gradedBy})</div>
+              )}
+            </div>
+          )}
+          
         </div>
       </div>
 
