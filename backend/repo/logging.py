8ad8ef2b6b097fcd70277dab0ac3cf022ba36c77
iu-capstone-ad <33985200+iu-capstone-ad@users.noreply.github.com>
2025-07-08 20:08:06 +0300

    fix typo in comment

--- backend/repo/logging.py
+++ backend/repo/logging.py
@@ -3,7 +3,7 @@ LOG_TRIGGER_COUNT = 100
 
 def sql_insert_log(db_cursor, tag, msg):
     db_cursor.execute("INSERT INTO logs (t, tag, msg) VALUES (now(), %s, %s)", (tag, msg))
-    # delete all logs on every LOG_TRIGGGER_COUNT inserted
+    # delete all logs on every LOG_TRIGGER_COUNT inserted
     db_cursor.execute("SELECT COUNT(*) FROM logs")
     log_count = db_cursor.fetchone()[0]
     if log_count % LOG_TRIGGER_COUNT == 0:
