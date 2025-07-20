3e4017a162220f10804129ef871eb03bdec4e6ae
Gleb Popov <g.popov@innopolis.university>
2025-07-20 15:52:29 +0300

    extra line added between functions

--- backend/repo/files.py
+++ backend/repo/files.py
@@ -2,6 +2,7 @@ def sql_download_attachment(storage_db_cursor, file_id):
     storage_db_cursor.execute("SELECT content FROM files WHERE id = %s", (file_id, ))
     return storage_db_cursor.fetchone()[0]
 
+
 def sql_select_attachment_metadata(db_cursor, file_id):
     db_cursor.execute("""
                       (SELECT fileid, filename, uploadtime FROM material_files WHERE fileid = %s)
