50810268f4bc1601379e96266c741fc6dfc633db
netimaaa <t.farizunov@innopolis.university>
2025-09-14 21:56:02 +0300

    Added Card ui component

---
+++ frontend/shared/ui/card/card.tsx
@@ -0,0 +1,24 @@
+import clsx from "clsx";
+import React, { HTMLAttributes } from "react";
+
+interface CardProps extends HTMLAttributes<HTMLDivElement> {
+  className?: string;
+}
+
+export const Card: React.FC<CardProps> = ({
+  className,
+  children,
+  ...props
+}) => {
+  return (
+    <div
+      className={clsx(
+        "min-w-10 min-h-10 p-2 border border-outline rounded-xl shadow-md",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
