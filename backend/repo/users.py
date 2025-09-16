0a7ea64c9a0910ed80bc2dccbf02dc3caba94aec
Gleb Popov <g.popov@innopolis.university>
2025-09-16 14:40:33 +0300

    return types specified in repo/users.py

--- backend/repo/users.py
+++ backend/repo/users.py
@@ -1,4 +1,5 @@
-from typing import Optional
+from typing import Optional, List, Tuple
+from uuid import UUID
 
 def sql_get_user_name(db_cursor, email: str) -> Optional[str]:
     db_cursor.execute("SELECT publicname FROM users WHERE email = %s", (email,))
@@ -28,7 +29,7 @@ def sql_update_password(db_cursor, email: str, hashed_new_password: str) -> None
     db_cursor.execute("UPDATE users SET passwordhash = %s WHERE email = %s", (hashed_new_password, email))
 
 
-def sql_select_single_teacher_courses(db_cursor, user_email: str):
+def sql_select_single_teacher_courses(db_cursor, user_email: str) -> List[UUID]:
     db_cursor.execute(
         "SELECT t.courseid FROM teaches t WHERE t.email = %s AND (SELECT COUNT(*) FROM teaches WHERE courseid = t.courseid) = 1",
         (user_email,),
@@ -36,24 +37,24 @@ def sql_select_single_teacher_courses(db_cursor, user_email: str):
     return [row[0] for row in db_cursor.fetchall()]
 
 
-def sql_delete_course(db_cursor, course_id: str):
+def sql_delete_course(db_cursor, course_id: str) -> None:
     db_cursor.execute("DELETE FROM courses WHERE courseid = %s", (course_id,))
 
 
-def sql_delete_user(db_cursor, user_email: str):
+def sql_delete_user(db_cursor, user_email: str) -> None:
     db_cursor.execute("DELETE FROM users WHERE email = %s", (user_email,))
 
 
-def sql_give_admin_permissions(db_cursor, user_email: str):
+def sql_give_admin_permissions(db_cursor, user_email: str) -> None:
     db_cursor.execute("UPDATE users SET isadmin = 't' WHERE email = %s", (user_email,))
 
 
-def sql_select_admins(db_cursor):
+def sql_select_admins(db_cursor) -> List[Tuple[str, str]]:
     db_cursor.execute("SELECT email, publicname FROM users WHERE isadmin")
     return db_cursor.fetchall()
 
 
-def sql_count_admins(db_cursor):
+def sql_count_admins(db_cursor) -> int:
     db_cursor.execute("SELECT COUNT(*) FROM users WHERE isadmin")
     return db_cursor.fetchone()[0]
 
@@ -63,6 +64,6 @@ def sql_admins_exist(db_cursor) -> bool:
     return db_cursor.fetchone()[0]
 
 
-def sql_select_all_users(db_cursor):
+def sql_select_all_users(db_cursor) -> List[Tuple[str, str]]:
     db_cursor.execute("SELECT email, publicname FROM users")
     return db_cursor.fetchall()
